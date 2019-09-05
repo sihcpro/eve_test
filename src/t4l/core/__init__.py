dcc827179a6d40a8c69f55bdaa85809caac8d9fb
sihc <tvquocchi@gmail.com>
2019-09-05 17:15:15 +0700

    add: hello route

--- src/t4l/core/__init__.py
+++ src/t4l/core/__init__.py
@@ -1,11 +1,24 @@
 from eve import Eve
+import logging
 
-my_settings = {
+defaults = {
     'MONGO_HOST': 'localhost',
     'MONGO_PORT': 27017,
     'MONGO_DBNAME': 'the_db_name',
     'DOMAIN': {'contacts': {}}
 }
 
-app = Eve(settings=my_settings)
+class ObjectServer(Eve):
+    def __init__(self):
+        super(ObjectServer, self).__init__(settings=defaults)
+        logging.info(self.__module__)
+
+        @self.route('/hello')
+        def hello_world():
+            return 'hello world!'
+
+
+app = ObjectServer()
+
+
 __all__ = ('app')
